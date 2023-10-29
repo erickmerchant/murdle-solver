c55265c91a82d2602d083b4680bb01ba13350353
erickmerchant <erickmerchant@gmail.com>
2023-10-29 00:21:42 -0400

    Latest version

--- murdle-solver.ts
+++ murdle-solver.ts
@@ -1,11 +1,12 @@
 import { parse } from "https://deno.land/std@0.204.0/flags/mod.ts";
 
-type Scenario = {
-  [key: number]: string;
-  yes?: boolean;
+type Dimension = string;
+
+type Clue = {
+  [key: string]: Dimension;
 };
 
-type Dimension = string;
+type Clues = Array<Clue>;
 
 const flags = parse(Deno.args, {
   string: ["dimension", "no", "yes"],
@@ -21,23 +22,58 @@ const dimension: Array<Array<Dimension>> = flags.dimension.map((
   a: Dimension,
 ): Array<Dimension> => a.split(","));
 
-const clues: Array<Scenario> = [
-  ...flags.no.map((value: string): [boolean, string] => [
-    false,
-    value,
-  ]),
-  ...flags.yes.map((value: string): [boolean, string] => [
-    true,
-    value,
-  ]),
-].map(([yes, value]: [boolean, string]): Scenario => {
-  const scenario: Scenario = { yes };
+const negatives: Clues = flags.no.map(mapClue);
+
+const positives: Clues = flags.yes.map(mapClue);
+
+const possibilities: Clues = dimension
+  .reduce(
+    (
+      res: Clues,
+      dimension: Array<Dimension>,
+      i: number,
+    ): Clues =>
+      dimension.map((d: Dimension) =>
+        i === 0
+          ? {
+            0: d,
+          }
+          : res.map((p: Clue): Clue => ({ [i]: d, ...p }))
+      )
+        .flat(),
+    [],
+  )
+  .filter(
+    filterClues(negatives, false),
+  )
+  .filter(
+    filterClues(positives, true),
+  );
+
+console.log(
+  possibilities.map((p: Clue): string => Object.values(p).join(" ")).join(
+    "\n",
+  ),
+);
+
+for (const c of positives) {
+  const match = possibilities.find((p: Clue): boolean =>
+    countSharedDimensions(p, c) === Object.keys(c).length
+  );
+
+  if (!match) {
+    warn(`no match found for ${Object.values(c).join(" ")}`);
+  }
+}
+
+function mapClue(value: string): Clue {
+  const clue: Clue = {};
 
   loop:
   for (const v of value.split(",")) {
     for (let i = 0; i < dimension.length; i++) {
       if (dimension[i].includes(v)) {
-        scenario[i] = v;
+        clue[i] = v;
 
         continue loop;
       }
@@ -46,64 +82,49 @@ const clues: Array<Scenario> = [
     throw new Error(`invalid dimension "${v}"`);
   }
 
-  return scenario;
-});
+  return clue;
+}
 
-const possibilities: Array<Scenario> = dimension
-  .reduce(
-    (
-      res: Array<Scenario>,
-      dimension: Array<Dimension>,
-      i: number,
-    ): Array<Scenario> =>
-      dimension.map((d: Dimension) =>
-        i === 0
-          ? {
-            0: d,
+function filterClues(
+  clues: Clues,
+  has: boolean,
+): (p: Clue) => boolean {
+  return (p: Clue): boolean => {
+    for (const c of clues) {
+      const i = countSharedDimensions(p, c);
+      const len = Object.keys(c).length;
+
+      switch (has) {
+        case false:
+          if (i == len) {
+            return false;
           }
-          : res.map((p: Scenario): Scenario => ({ [i]: d, ...p }))
-      )
-        .flat(),
-    [],
-  )
-  .filter(
-    (
-      p: Scenario,
-    ): boolean => {
-      for (const c of clues) {
-        let i = 0;
-        const len = Object.keys(c).length - 1;
+          break;
 
-        for (const k in c) {
-          if (p[k] === c[k]) {
-            i += 1;
+        case true:
+          if ((i > 0 || len === 1) && i !== len) {
+            return false;
           }
-        }
-
-        switch (c.yes) {
-          case false:
-            if (i == len) {
-              return false;
-            }
-
-            break;
-
-          case true:
-            if (c.yes) {
-              if ((i > 0 || len === 1) && i !== len) {
-                return false;
-              }
-            }
-            break;
-        }
+          break;
       }
+    }
 
-      return true;
-    },
-  );
+    return true;
+  };
+}
 
-console.log(
-  possibilities.map((p: Scenario): string => Object.values(p).join(" ")).join(
-    "\n",
-  ),
-);
+function countSharedDimensions(a: Clue, b: Clue) {
+  let i = 0;
+
+  for (const k in a) {
+    if (b[k] === a[k]) {
+      i += 1;
+    }
+  }
+
+  return i;
+}
+
+function warn(msg: string) {
+  console.warn(`%c${msg}`, "color: yellow");
+}
